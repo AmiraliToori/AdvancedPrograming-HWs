f83e19ba0832c6d6f41d646c4c7b82a76ee43865
AmiraliToori <master.masterblocker@gmail.com>
2024-05-26 09:37:15 +0330

    move some object to models folder

--- HW9/Q2/main.py
+++ HW9/Q2/main.py
@@ -1,7 +1,8 @@
 
 import pygame as pg
-from models import objects
-from models import game_screen
+from models import game_screen, bullet, target, player
+import math
+from icecream import ic
 
 # Display option
 SCREEN_WIDTH = 800
@@ -9,105 +10,95 @@ SCREEN_HEIGHT = 600
 
 COLOR_BACKGROUND = 'white'
 
-# Player
-PLAYER = objects.Player()
 
-# Target
-TARGET = objects.Target()
 
-# Score
-SCORE_FONT_SIZE = 20
-SCORE_FONT_COLOR = "black"
-SCORE_FONT_COLOR_BACKGROUND = "white"
-SCORE_FONT_X = 700
-SCORE_FONT_Y = 5
-
-# Lives
-LIVES_FONT_SIZE = 30
-LIVES_FONT_COLOR = "black"
-LIVES_FONT_COLOR_BACKGROUND = "white"
-LIVES_FONT_X = 0
-LIVES_FONT_Y = 570
-
-# Velocity
-
-VELOCITY_FONT_SIZE = 20
-VELOCITY_FONT_COLOR = "black"
-VELOCITY_FONT_COLOR_BACKGROUND = "white"
-VELOCITY_FONT_X = 670
-VELOCITY_FONT_Y = 580
 
 def main():
-
-    pg.init()
-    screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    
     run = True
     screen_num = 1
-    throw = True
-    velocity = 0.5
+    velocity_value = 2
     
-    while run:
-        match screen_num:
-            case 1:
-                screen.fill(COLOR_BACKGROUND)
-                PLAYER.draw(screen)
-                TARGET.draw(screen)
-                game_screen.draw_timer(screen)
-                game_screen.draw_line(screen)
-                
-                game_screen.draw_text(f"SCORE: {PLAYER.score}",
-                                      SCORE_FONT_SIZE,
-                                      SCORE_FONT_COLOR,
-                                      SCORE_FONT_COLOR_BACKGROUND,
-                                      screen,
-                                      SCORE_FONT_X,
-                                      SCORE_FONT_Y)
-                
-                game_screen.draw_text(f"LIVES: {PLAYER.lives}",
-                                      LIVES_FONT_SIZE,
-                                      LIVES_FONT_COLOR,
-                                      LIVES_FONT_COLOR_BACKGROUND,
-                                      screen,
-                                      LIVES_FONT_X,
-                                      LIVES_FONT_Y)
-                
-                game_screen.draw_text(f"VELOCITY: {velocity}",
-                                      VELOCITY_FONT_SIZE,
-                                      VELOCITY_FONT_COLOR,
-                                      VELOCITY_FONT_COLOR_BACKGROUND,
-                                      screen,
-                                      VELOCITY_FONT_X,
-                                      VELOCITY_FONT_Y)
-                
-                if not throw == True:
-                    PLAYER.throw(screen, velocity)
-                    
-                
-                
-                    
+    pg.init()
+    screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    clock = pg.time.Clock()
+    
+    target_ball = target.Target(screen)
+    bullet_ball = bullet.Bullet(screen, velocity_value)
+    gamer = player.Player(screen)
+    balls = []
+    
+    
 
-        
-        keys = pg.key.get_pressed()
-        
-        if keys[pg.K_SPACE]:
-            throw = False
-        
-        
+    while run:
+        clock.tick(60)
+        match screen_num:
+            
+            case 1:
+                
+                screen.fill(COLOR_BACKGROUND)
+                
+                time = pg.time.get_ticks() // 1000
+                main_screen = game_screen.GameScreen(screen, velocity_value, time, gamer)
+                main_screen.draw()
+                gamer.draw()
+                target_ball.draw()
+                
+                
+                
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
                 
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_v:
-                    velocity += 0.1
+                    velocity_value += 0.1
                 elif event.key == pg.K_c:
-                    velocity -= 0.1
+                    velocity_value -= 0.1
                 elif event.key == pg.K_q:
                     run = False
+        
 
+        if pg.mouse.get_pressed()[0] == True and len(balls) == 0:
+            balls.append(bullet.Bullet(screen, velocity_value))
+        
+        for ball in balls:
+            time_throw = pg.time.get_ticks()
+            ball.draw(screen)
+            ball.update(time_throw)
+            if not screen.get_rect().collidepoint(ball.pos):
+                balls.remove(ball)
+            
+        
         pg.display.flip()
-
+        
+        
 if __name__ == "__main__":
     main()
     pg.quit()
+    
+    
+    
+    
+    
+
+
+            # dest_x, dest_y = pg.mouse.get_pos()
+
+            # hypothesis = math.sqrt((dest_x - gamer.x)**2 + (dest_y - gamer.y)**2)
+            # opposite_edge = math.sqrt(0 + (dest_y - gamer.y)**2)
+
+            # angle_degree = math.degrees(math.acos(opposite_edge / hypothesis))
+
+            # print(angle_degree)
+
+            # # u = velocity
+            # alpha = np.radians(angle_degree)
+            # max_range = velocity**2 * np.sin(2*alpha)/gravity
+            # max_height = velocity**2 * (np.sin(alpha))**2 / (2*gravity)
+            # x_array = np.linspace(0, max_range, 20)
+            # y_array = x_array * np.tan(alpha) - (1/2)* (gravity*x_array**2)/(velocity**2*(np.cos(alpha))**2)
+
+            # print(x_array)
+            # print(y_array)
+            
+    
