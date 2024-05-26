ee1c7a39c17404e6e5a4b3a41034b85d2b8bac63
AmiraliToori <master.masterblocker@gmail.com>
2024-05-26 09:25:49 +0330

    some changes to objects thing

--- HW9/Q2/models/objects.py
+++ HW9/Q2/models/objects.py
@@ -1,87 +1,76 @@
 
-
-
 import pygame as pg
-from random import randint
-
-# Player Icon 
-PLAYER_ICON_PATH = "material/icons8-human-50.png"
-PLAYER_IMG = pg.image.load(PLAYER_ICON_PATH)
-
-# Player position coordinates
-PLAYER_X = 375
-PLAYER_Y = 550
-
-# Target Icon
-TARGET_ICON_PATH = "material/icons8-circle-50.png"
-TARGET_IMG = pg.image.load(TARGET_ICON_PATH)
-
-# Ball Icon
-BALL_ICON_PATH = "material/icons8-ball-30.png"
-BALL_IMG = pg.image.load(BALL_ICON_PATH)
 
 
-class Ball:
+# Font
+FONT_PATH = "material/Material_Sans.ttf"
+
+# Anti alias option
+ANTI_ALIAS_OPTION = True
+
+
+class DrawText:
     
-    def __init__(self, mass) -> None:
-        self.x = PLAYER_X 
-        self.y = PLAYER_Y
-        self.mass = mass
-    
-    def throw(self, screen, velocity) -> None:
-        if self.y >= 30:
-            self.y -= velocity
-            
-            if self.y == 30:
-                pass
-        else:
-            self.y 
-        screen.blit(BALL_IMG, (self.x, self.y))    
-            
-    
-    
-    
-
-ball = Ball()
-
-
-class Player:
-    
-    def __init__(self) -> None:
-        self.x = PLAYER_X
-        self.y = PLAYER_Y
-        self.lives = 3
-        self.is_lose = False
-        self.score = 0
+    def __init__(self,
+                text_input: str,
+                font_size: int,
+                text_color: tuple | str,
+                background_color: tuple | str,
+                screen,
+                x_pos: int,
+                y_pos: int) -> None:
         
-    def throw(self, screen, velocity) -> None:
-        ball.throw(screen, velocity)
-    
-    def draw(self, screen) -> None:
-        screen.blit(PLAYER_IMG, (self.x, self.y))
-
-
-
-class Target:
-    
-    def __init__(self) -> None:
-        self.x = randint(0, 750)
-        self.y = randint(40, 300)
-        self.is_dead = True
+        self.text_input = text_input
+        self.font_size = font_size
+        self.text_color = text_color
+        self.background_color = background_color
+        self.screen = screen
+        self.x_pos = x_pos
+        self.y_pos = y_pos
         
-    def draw(self, screen) -> None:
-        screen.blit(TARGET_IMG, (self.x, self.y))
+    def draw(self) -> None:
+        font = pg.font.Font(FONT_PATH, self.font_size)
+        text = font.render(self.text_input, ANTI_ALIAS_OPTION, self.text_color, self.background_color)
+        self.screen.blit(text, (self.x_pos, self.y_pos))
+
+
+
+class DrawLine:
+    
+    
+    def __init__(self,
+                 screen,
+                 color: str,
+                 start_line_pos,
+                 end_line_pos):
+        self.screen = screen
+        self.color = color
+        self.start_line_pos = start_line_pos
+        self.end_line_pos = end_line_pos
+    
+    def draw(self):
+        pg.draw.line(self.screen,
+                     self.color,
+                     self.start_line_pos,
+                     self.end_line_pos)
         
-    def collision(self):
-        self.is_dead = False
-
-
-
-
-
+        
+class DrawIcon:
+    
+    
+    def __init__(self, screen, source, x_pos, y_pos) -> None:
+        self.screen = screen
+        self.source = source
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        
+    def draw(self) -> None:
+        self.screen.blit(self.source,
+                         (self.x_pos,
+                         self.y_pos))
+        
 
 
 
     
-
-
+    
