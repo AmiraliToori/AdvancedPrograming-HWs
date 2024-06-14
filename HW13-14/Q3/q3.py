26ca2648e7379b64d7db049646bfd7b98ba9e549
AmiraliToori <master.masterblocker@gmail.com>
2024-06-14 18:47:05 +0330

    q3.py
    A better representation for wireshark.

--- HW13-14/Q3/q3.py
+++ HW13-14/Q3/q3.py
@@ -1,27 +1,33 @@
 
 import matplotlib.pyplot as plt
 import re
-import numpy as np
 
 file_path = "text.txt"
 
 with open(file_path, "r") as f:
     text = f.read()
     
-pattern = re.compile(r"[0-9]+[\.]{1}[\d]{6,}")
+pattern = re.compile(r"[\-]{0,1}[0-9]+[\.]{1}[\d]{6,}")
 
 temp = pattern.findall(text)
 
 times_lst = [int(float(value)) for value in temp]
 
+dictionary = {str(number):times_lst.count(number) for number in times_lst}
 
-fig, ax = plt.subplots(figsize = (10, 6))
+seconds = list(dictionary.keys())
+numbers = list(dictionary.values())
 
-ax.hist(times_lst)
-ax.set_xlabel("Second")
-ax.set_ylabel("Number of packets")
+
+fig, ax = plt.subplots(figsize = (20, 10))
+ax.plot(seconds, numbers, marker = "o", linewidth = 2)
+ax.set_xlabel("Second", fontsize = 15)
+ax.set_ylabel("Number of packets", fontsize = 15)
 ax.set_title("Number of packets which record per second")
-ax.set_xticks([x * 10**8 for x in np.arange(0, 2.8, 0.1)])
-ax.grid(True, axis = 'y')
+ax.tick_params(axis = "x", rotation = 90, labelsize = 8)
+ax.grid(True, axis = 'both', linestyle = "--")
+
+for count, value in enumerate(numbers):
+    ax.text(count, value, str(value), ha = "right", va = "bottom")
 
 plt.show()
